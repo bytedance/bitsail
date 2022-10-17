f81f3fd460cefb5c307a61e1a745af76df92ef14
liupeng.96 <96pengpeng@gmail.com>
2022-10-17 21:41:02 +0800

    Add licenses.

--- bitsail-connectors/bitsail-connectors-legacy/bitsail-connector-larksheet/src/main/java/com/bytedance/bitsail/connector/legacy/larksheet/option/LarkSheetReaderOptions.java
+++ bitsail-connectors/bitsail-connectors-legacy/bitsail-connector-larksheet/src/main/java/com/bytedance/bitsail/connector/legacy/larksheet/option/LarkSheetReaderOptions.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.bytedance.bitsail.connector.legacy.larksheet.option;
 
 import com.alibaba.fastjson.TypeReference;
@@ -54,42 +71,42 @@ public interface LarkSheetReaderOptions extends ReaderOptions.BaseReaderOptions 
       .onlyReference(new TypeReference<List<Integer>>(){});
 
   /**
-   *
+   * app_id for generating token.
    */
   ConfigOption<String> APP_ID =
       key(READER_PREFIX + "lark_app_id")
           .noDefaultValue(String.class);
 
   /**
-   *
+   * app_secret for generating token.
    */
   ConfigOption<String> APP_SECRET =
       key(READER_PREFIX + "lark_app_secret")
           .noDefaultValue(String.class);
 
   /**
-   *
+   * Host of lark open api.
    */
   ConfigOption<String> OPEN_API_HOST =
       key(READER_PREFIX + "lark_open_api_host")
           .defaultValue("https://open.feishu.cn");
 
   /**
-   *
+   * API for app_access_token.
    */
   ConfigOption<String> APP_ACCESS_TOKEN_API =
       key(READER_PREFIX + "lark_app_access_token_api")
           .defaultValue("/open-apis/auth/v3/app_access_token/internal");
 
   /**
-   *
+   * API for get metadata.
    */
   ConfigOption<String> META_INFO_API_FORMAT =
       key(READER_PREFIX + "lark_meta_info_api_format")
           .defaultValue("/open-apis/sheet/v2/spreadsheets/%s/metainfo");
 
   /**
-   *
+   * API for get a range of data from sheet.
    */
   ConfigOption<String> SINGLE_RANGE_API_FORMAT =
       key(READER_PREFIX + "lark_sheet_single_range_api_format")
