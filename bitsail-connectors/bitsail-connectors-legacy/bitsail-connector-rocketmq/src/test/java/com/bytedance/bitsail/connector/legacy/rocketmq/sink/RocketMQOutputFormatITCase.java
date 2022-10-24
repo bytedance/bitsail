9c04a25879d59bf3cf950b5a2b2c6238905aa908
liupeng.96 <96pengpeng@gmail.com>
2022-10-24 20:32:46 +0800

    Optimize writer itcase test.

--- bitsail-connectors/bitsail-connectors-legacy/bitsail-connector-rocketmq/src/test/java/com/bytedance/bitsail/connector/legacy/rocketmq/sink/RocketMQOutputFormatITCase.java
+++ bitsail-connectors/bitsail-connectors-legacy/bitsail-connector-rocketmq/src/test/java/com/bytedance/bitsail/connector/legacy/rocketmq/sink/RocketMQOutputFormatITCase.java
@@ -23,32 +23,82 @@ import com.bytedance.bitsail.connector.legacy.rocketmq.option.RocketMQWriterOpti
 import com.bytedance.bitsail.test.connector.test.EmbeddedFlinkCluster;
 import com.bytedance.bitsail.test.connector.test.utils.JobConfUtils;
 
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Need to start rocket service first.
  */
 @Ignore
 public class RocketMQOutputFormatITCase {
+  private static final Logger LOG = LoggerFactory.getLogger(RocketMQOutputFormatITCase.class);
 
   private static final int TOTAL_SEND_COUNT = 300;
-  private static final String topicName = "test_topic";
+
+  private static final String NAME_SERVER_ADDRESS = "127.0.0.1:9876";
+  private static final String TOPIC_NAME = "test_topic";
+  private static final String PRODUCER_GROUP = "test_producer_group";
+  private static final String TAG = "itcase_test";
 
   @Test
   public void testFakeToRocketMQ() throws Exception {
     BitSailConfiguration configuration = JobConfUtils.fromClasspath("fake_to_rocketmq.json");
     updateConfiguration(configuration);
     EmbeddedFlinkCluster.submitJob(configuration);
+
+    Queue<MessageExt> messages = consumeTopic();
+    Assert.assertEquals(TOTAL_SEND_COUNT, messages.size());
   }
 
   protected void updateConfiguration(BitSailConfiguration jobConfiguration) {
     jobConfiguration.set(FakeReaderOptions.TOTAL_COUNT, TOTAL_SEND_COUNT);
-    jobConfiguration.set(RocketMQWriterOptions.NAME_SERVER_ADDRESS, "127.0.0.1:9876");
-    jobConfiguration.set(RocketMQWriterOptions.TOPIC, topicName);
-    jobConfiguration.set(RocketMQWriterOptions.PRODUCER_GROUP, "test_producer_group");
-    jobConfiguration.set(RocketMQWriterOptions.TAG, "itcase_test");
+    jobConfiguration.set(RocketMQWriterOptions.NAME_SERVER_ADDRESS, NAME_SERVER_ADDRESS);
+    jobConfiguration.set(RocketMQWriterOptions.TOPIC, TOPIC_NAME);
+    jobConfiguration.set(RocketMQWriterOptions.PRODUCER_GROUP, PRODUCER_GROUP);
+    jobConfiguration.set(RocketMQWriterOptions.TAG, TAG);
     jobConfiguration.set(RocketMQWriterOptions.PARTITION_FIELDS, "id");
     jobConfiguration.set(RocketMQWriterOptions.ENABLE_BATCH_FLUSH, false);
+  }
+
+  private Queue<MessageExt> consumeTopic() throws Exception {
+    DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("test_consumer");
+    consumer.setNamesrvAddr(NAME_SERVER_ADDRESS);
+    consumer.subscribe(TOPIC_NAME, TAG);
+    consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+    Queue<MessageExt> messageQ = new ConcurrentLinkedQueue<>();
+
+    consumer.registerMessageListener(
+        (MessageListenerConcurrently)
+            (msgs, context) -> {
+              messageQ.addAll(msgs);
+              return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            });
+    try {
+      consumer.start();
+    } catch (MQClientException e) {
+      LOG.info("send message failed. {}", e.toString());
+    }
+    Thread.sleep(5000);
+    consumer.shutdown();
+
+    for (MessageExt msg : messageQ) {
+      LOG.debug(new String(msg.getBody()));
+    }
+
+    LOG.info("Total get {} messages.", messageQ.size());
+    return messageQ;
   }
 }
