a9861fa136c9a56765d04884ed94e1dae66eff1b
Sam Chu <ysamchu@gmail.com>
2022-10-24 20:48:28 -0700

    Add licenses

--- bitsail-connectors/bitsail-connectors-legacy/bitsail-connector-jdbc/src/main/java/com/bytedance/bitsail/connector/legacy/jdbc/source/OracleInputFormat.java
+++ bitsail-connectors/bitsail-connectors-legacy/bitsail-connector-jdbc/src/main/java/com/bytedance/bitsail/connector/legacy/jdbc/source/OracleInputFormat.java
@@ -13,10 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Original Files: apache/flink(https://github.com/apache/flink)
- * Copyright: Copyright 2014-2022 The Apache Software Foundation
- * SPDX-License-Identifier: Apache License 2.0
  */
 
 package com.bytedance.bitsail.connector.legacy.jdbc.source;
