589807d71e170cca2d7b42242255a2c54e93a815
haoke <haoke@bytedance.com>
2023-02-01 21:13:29 +0800

    [BitSail][Fix]add return for split enumerator.

--- bitsail-cores/bitsail-core-flink-bridge/src/main/java/com/bytedance/bitsail/core/flink/bridge/reader/delegate/DelegateFlinkSourceSplitEnumerator.java
+++ bitsail-cores/bitsail-core-flink-bridge/src/main/java/com/bytedance/bitsail/core/flink/bridge/reader/delegate/DelegateFlinkSourceSplitEnumerator.java
@@ -142,6 +142,7 @@ public class DelegateFlinkSourceSplitEnumerator<SplitT extends SourceSplit,
     if (sourceEvent instanceof DelegateSourceEvent) {
       coordinator.handleSourceEvent(subtaskId,
           ((DelegateSourceEvent) sourceEvent).getSourceEvent());
+      return;
     }
     throw BitSailException.asBitSailException(CommonErrorCode.RUNTIME_ERROR,
         "Source event in not instanceof delegate source event, it's always runtime bug.");
