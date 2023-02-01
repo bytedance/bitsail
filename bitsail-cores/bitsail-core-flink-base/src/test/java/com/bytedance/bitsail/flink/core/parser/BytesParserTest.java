86d1c3348bd227fab03cf0e947f6b559b3decf70
liupeng.96 <96pengpeng@gmail.com>
2023-02-01 16:13:54 +0800

    Add ut for bitsail-core-flink-base

--- bitsail-cores/bitsail-core-flink-base/src/test/java/com/bytedance/bitsail/flink/core/parser/BytesParserTest.java
+++ bitsail-cores/bitsail-core-flink-base/src/test/java/com/bytedance/bitsail/flink/core/parser/BytesParserTest.java
@@ -18,13 +18,20 @@ package com.bytedance.bitsail.flink.core.parser;
 
 import com.bytedance.bitsail.common.column.Column;
 import com.bytedance.bitsail.common.column.ColumnCast;
+import com.bytedance.bitsail.common.column.ListColumn;
 import com.bytedance.bitsail.common.column.LongColumn;
+import com.bytedance.bitsail.common.column.MapColumn;
 import com.bytedance.bitsail.common.column.StringColumn;
 import com.bytedance.bitsail.common.configuration.BitSailConfiguration;
 import com.bytedance.bitsail.common.option.CommonOptions;
+import com.bytedance.bitsail.flink.core.typeinfo.ListColumnTypeInfo;
+import com.bytedance.bitsail.flink.core.typeinfo.MapColumnTypeInfo;
 import com.bytedance.bitsail.flink.core.typeinfo.PrimitiveColumnTypeInfo;
 
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
 import org.apache.flink.types.Row;
 import org.junit.Assert;
@@ -32,12 +39,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 
 public class BytesParserTest {
 
@@ -78,6 +87,9 @@ public class BytesParserTest {
     Assert.assertEquals(fieldVal.toString(), column.getRawData());
 
     column = bytesParser.createBasicColumn(PrimitiveColumnTypeInfo.STRING_COLUMN_TYPE_INFO, fieldVal);
+    Assert.assertEquals(fieldVal.toString(), column.getRawData());
+
+    column = bytesParser.createColumn(PrimitiveColumnTypeInfo.STRING_COLUMN_TYPE_INFO, fieldVal);
     Assert.assertEquals(fieldVal.toString(), column.getRawData());
   }
 
@@ -227,6 +239,92 @@ public class BytesParserTest {
 
     fieldValue = new boolean[] {true, false, true};
     ret = bytesParser.convertArrayToList(fieldValue);
-    Assert.assertArrayEquals(ret);
+    checkListEqual(fieldValue, ret, 3, (o, i) -> ((boolean[]) o)[i]);
+
+    fieldValue = new byte[] {1, 2, 3};
+    ret = bytesParser.convertArrayToList(fieldValue);
+    checkListEqual(fieldValue, ret, 3, (o, i) -> ((byte[]) o)[i]);
+
+    fieldValue = new char[] {'a', 'b', 'c'};
+    ret = bytesParser.convertArrayToList(fieldValue);
+    checkListEqual(fieldValue, ret, 3, (o, i) -> ((char[]) o)[i]);
+
+    fieldValue = new short[] {2, 4, 8};
+    ret = bytesParser.convertArrayToList(fieldValue);
+    checkListEqual(fieldValue, ret, 3, (o, i) -> ((short[]) o)[i]);
+
+    fieldValue = new int[] {12, 14, 18};
+    ret = bytesParser.convertArrayToList(fieldValue);
+    checkListEqual(fieldValue, ret, 3, (o, i) -> ((int[]) o)[i]);
+
+    fieldValue = new long[] {10002, 10004, 10008};
+    ret = bytesParser.convertArrayToList(fieldValue);
+    checkListEqual(fieldValue, ret, 3, (o, i) -> ((long[]) o)[i]);
+
+    fieldValue = new float[] {10.002f, 10.004f, 10.008f};
+    ret = bytesParser.convertArrayToList(fieldValue);
+    checkListEqual(fieldValue, ret, 3, (o, i) -> ((float[]) o)[i]);
+
+    fieldValue = new double[] {10.002d, 10.004d, 10.008d};
+    ret = bytesParser.convertArrayToList(fieldValue);
+    checkListEqual(fieldValue, ret, 3, (o, i) -> ((double[]) o)[i]);
+
+    fieldValue = new String[] {"10.002d", "10.004d", "10.008d"};
+    ret = bytesParser.convertArrayToList(fieldValue);
+    checkListEqual(fieldValue, ret, 3, (o, i) -> ((String[]) o)[i]);
+  }
+
+  private static void checkListEqual(Object expected,
+                                     List<?> actual,
+                                     int expectedSize,
+                                     BiFunction<Object, Integer, ?> expectedElementGetter) {
+    Assert.assertTrue(actual != null && actual.size() == expectedSize);
+
+    for (int i = 0; i < expectedSize; ++i) {
+      Object expectedElem = expectedElementGetter.apply(expected, i);
+      Object actualElem = actual.get(i);
+
+      Assert.assertFalse((expectedElem == null) ^ (actualElem == null));
+      if (expectedElem == null) {
+        continue;
+      }
+      Assert.assertEquals(expectedElem, actualElem);
+    }
+  }
+
+  @Test
+  public void testCreateListColumn() {
+    TypeInformation<?> typeInfo = new ListColumnTypeInfo<>(StringColumn.class);
+    List<String> fieldVal = Lists.newArrayList("abc", "def", "gh");
+    ListColumn<?> listColumn = bytesParser.createListColumn(typeInfo, fieldVal);
+    for (int i = 0; i < 3; ++i) {
+      Assert.assertEquals(fieldVal.get(i), listColumn.get(i).asString());
+    }
+
+    listColumn = bytesParser.createListColumn(typeInfo, fieldVal);
+    for (int i = 0; i < 3; ++i) {
+      Assert.assertEquals(fieldVal.get(i), listColumn.get(i).asString());
+    }
+  }
+
+  @Test
+  public void testCreateMapColumn() {
+    MapColumnTypeInfo<?,?> typeInfo = new MapColumnTypeInfo<>(LongColumn.class, StringColumn.class);
+    Map<Long, String> fieldVal = ImmutableMap.of(
+        1L, "val_1",
+        2L, "val_2",
+        3L, "val_3"
+    );
+    MapColumn<?,?> column = bytesParser.createMapColumn(typeInfo, fieldVal);
+    Map<?, ?> actualMap = column.getRawData();
+    for (long i = 0; i < 3; ++i) {
+      Assert.assertEquals(fieldVal.get(i), actualMap.get(BigInteger.valueOf(i)));
+    }
+
+    column = (MapColumn<?, ?>) bytesParser.createColumn(typeInfo, fieldVal);
+    actualMap = column.getRawData();
+    for (long i = 0; i < 3; ++i) {
+      Assert.assertEquals(fieldVal.get(i), actualMap.get(BigInteger.valueOf(i)));
+    }
   }
 }
