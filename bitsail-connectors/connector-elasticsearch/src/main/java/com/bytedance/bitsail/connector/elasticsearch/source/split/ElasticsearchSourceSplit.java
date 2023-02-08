297910c36f6e26617840edde0987424316d46a1b
liuxiao <liuxiao2103@qq.com>
2023-02-08 22:51:35 +0800

    fix format

--- bitsail-connectors/connector-elasticsearch/src/main/java/com/bytedance/bitsail/connector/elasticsearch/source/split/ElasticsearchSourceSplit.java
+++ bitsail-connectors/connector-elasticsearch/src/main/java/com/bytedance/bitsail/connector/elasticsearch/source/split/ElasticsearchSourceSplit.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.bytedance.bitsail.connector.elasticsearch.source.split;
 
 import com.bytedance.bitsail.base.connector.reader.v1.SourceSplit;
