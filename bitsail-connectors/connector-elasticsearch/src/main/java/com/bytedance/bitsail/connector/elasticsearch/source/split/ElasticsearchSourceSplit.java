7bc95d938c22ee253d0185af9904da68e31b80a4
liuxiao <liuxiao2103@qq.com>
2023-02-08 22:09:56 +0800

    update license

--- bitsail-connectors/connector-elasticsearch/src/main/java/com/bytedance/bitsail/connector/elasticsearch/source/split/ElasticsearchSourceSplit.java
+++ bitsail-connectors/connector-elasticsearch/src/main/java/com/bytedance/bitsail/connector/elasticsearch/source/split/ElasticsearchSourceSplit.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Bytedance Ltd. and/or its affiliates.
+ * Copyright 2022-2023 Bytedance Ltd. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.bytedance.bitsail.connector.elasticsearch.source.split;
 
 import com.bytedance.bitsail.base.connector.reader.v1.SourceSplit;
