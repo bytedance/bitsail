2f17d78fff0e9d519fb883abc4b4af0d56ec6546
garyli1019 <liyanjia.gary@bytedance.com>
2023-02-13 02:44:22 +0000

    Deploying to gh-pages from @ bytedance/bitsail@bf1d0d641840092eb26404f7b9946b2f7c66587b 🚀

---
+++ assets/hive-example.html.37fbb20e.js
@@ -0,0 +1,53 @@
+import{_ as t}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as e,c as o,a as n,b as s,d as p,w as i,e as l,r}from"./app.7342ef47.js";const c={},u=n("h1",{id:"hive-connector-example",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#hive-connector-example","aria-hidden":"true"},"#"),s(" Hive connector example")],-1),d=l(`<p>The following shows test hive table with user parameters and how to read/write it with hive connectors.</p><h2 id="test-hive-table-information" tabindex="-1"><a class="header-anchor" href="#test-hive-table-information" aria-hidden="true">#</a> Test hive table information</h2><ul><li>Example hive info: <ul><li><p>hive database name: test_db</p></li><li><p>hive table name: test_table</p></li><li><p>metastore uri: <code>thrift://localhost:9083</code></p></li><li><p>partition: p_date</p></li><li><p>DDL:</p><table><thead><tr><th>filed name</th><th>field type</th><th>description</th></tr></thead><tbody><tr><td>id</td><td>bigint</td><td>-</td></tr><tr><td>state</td><td>string</td><td>-</td></tr><tr><td>county</td><td>string</td><td>-</td></tr><tr><td>p_date</td><td>string</td><td>partition tield</td></tr></tbody></table></li></ul></li></ul><h2 id="hive-reader-example" tabindex="-1"><a class="header-anchor" href="#hive-reader-example" aria-hidden="true">#</a> Hive reader example</h2><p>Configuration for reading the above test hive table:</p><div class="language-json line-numbers-mode" data-ext="json"><pre class="language-json"><code><span class="token punctuation">{</span>
+   <span class="token property">&quot;job&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+      <span class="token property">&quot;reader&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+         <span class="token property">&quot;class&quot;</span><span class="token operator">:</span> <span class="token string">&quot;com.bytedance.bitsail.connector.legacy.hive.source.HiveInputFormat&quot;</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;columns&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+            <span class="token punctuation">{</span>
+               <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;id&quot;</span><span class="token punctuation">,</span>
+               <span class="token property">&quot;type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;bigint&quot;</span>
+            <span class="token punctuation">}</span><span class="token punctuation">,</span>
+            <span class="token punctuation">{</span>
+               <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;state&quot;</span><span class="token punctuation">,</span>
+               <span class="token property">&quot;type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;string&quot;</span>
+            <span class="token punctuation">}</span><span class="token punctuation">,</span>
+            <span class="token punctuation">{</span>
+               <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;county&quot;</span><span class="token punctuation">,</span>
+               <span class="token property">&quot;type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;string&quot;</span>
+            <span class="token punctuation">}</span>
+         <span class="token punctuation">]</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;db_name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;test_db&quot;</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;table_name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;test_table&quot;</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;metastore_properties&quot;</span><span class="token operator">:</span> <span class="token string">&quot;{\\&quot;hive.metastore.uris\\&quot;:\\&quot;thrift://localhost:9083\\&quot;}&quot;</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;partition&quot;</span><span class="token operator">:</span> <span class="token string">&quot;p_date=20220101&quot;</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;reader_parallelism_num&quot;</span><span class="token operator">:</span> <span class="token number">1</span>
+      <span class="token punctuation">}</span>
+   <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="hive-writer-example" tabindex="-1"><a class="header-anchor" href="#hive-writer-example" aria-hidden="true">#</a> Hive writer example</h2><p>Configuration for writing the above test hive table:</p><div class="language-json line-numbers-mode" data-ext="json"><pre class="language-json"><code><span class="token punctuation">{</span>
+   <span class="token property">&quot;job&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+      <span class="token property">&quot;writer&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+         <span class="token property">&quot;class&quot;</span><span class="token operator">:</span> <span class="token string">&quot;com.bytedance.bitsail.connector.legacy.hive.sink.HiveOutputFormat&quot;</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;columns&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+            <span class="token punctuation">{</span>
+               <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;id&quot;</span><span class="token punctuation">,</span>
+               <span class="token property">&quot;type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;bigint&quot;</span>
+            <span class="token punctuation">}</span><span class="token punctuation">,</span>
+            <span class="token punctuation">{</span>
+               <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;state&quot;</span><span class="token punctuation">,</span>
+               <span class="token property">&quot;type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;string&quot;</span>
+            <span class="token punctuation">}</span><span class="token punctuation">,</span>
+            <span class="token punctuation">{</span>
+               <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;county&quot;</span><span class="token punctuation">,</span>
+               <span class="token property">&quot;type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;string&quot;</span>
+            <span class="token punctuation">}</span>
+         <span class="token punctuation">]</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;db_name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;test_db&quot;</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;table_name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;test_table&quot;</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;metastore_properties&quot;</span><span class="token operator">:</span> <span class="token string">&quot;{\\&quot;hive.metastore.uris\\&quot;:\\&quot;thrift://localhost:9083\\&quot;}&quot;</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;partition&quot;</span><span class="token operator">:</span> <span class="token string">&quot;p_date=20220101&quot;</span><span class="token punctuation">,</span>
+         <span class="token property">&quot;writer_parallelism_num&quot;</span><span class="token operator">:</span> <span class="token number">1</span>
+      <span class="token punctuation">}</span>
+   <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,9);function v(k,m){const a=r("RouterLink");return e(),o("div",null,[u,n("p",null,[s("Parent document: "),p(a,{to:"/en/documents/connectors/hive/hive.html"},{default:i(()=>[s("Hive connector")]),_:1})]),d])}const h=t(c,[["render",v],["__file","hive-example.html.vue"]]);export{h as default};
---
+++ assets/rocketmq-example.html.82491e00.js
@@ -0,0 +1,43 @@
+import{_ as t}from"./_plugin-vue_export-helper.cdc0426e.js";import{o,c as e,a as n,b as s,d as p,w as c,e as i,r as u}from"./app.7342ef47.js";const l={},r=n("h1",{id:"rocketmq-连接器示例",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#rocketmq-连接器示例","aria-hidden":"true"},"#"),s(" RocketMQ 连接器示例")],-1),d=i(`<h2 id="rocketmq-写连接器" tabindex="-1"><a class="header-anchor" href="#rocketmq-写连接器" aria-hidden="true">#</a> RocketMQ 写连接器</h2><p>假设我们在本地启动了一个RocketMQ服务，其name server地址为 &quot;127.0.0.1:9876&quot;, 并且我们在其中创建了一个名为 &quot;test_topic&quot; 的topic。</p><p>那么我们可以使用如下的配置文档写入上述topic：</p><div class="language-json line-numbers-mode" data-ext="json"><pre class="language-json"><code><span class="token punctuation">{</span>
+   <span class="token property">&quot;job&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+     <span class="token property">&quot;writer&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+       <span class="token property">&quot;class&quot;</span><span class="token operator">:</span> <span class="token string">&quot;com.bytedance.bitsail.connector.legacy.rocketmq.sink.RocketMQOutputFormat&quot;</span><span class="token punctuation">,</span>
+       <span class="token property">&quot;name_server_address&quot;</span><span class="token operator">:</span> <span class="token string">&quot;127.0.0.1:9876&quot;</span><span class="token punctuation">,</span>
+       <span class="token property">&quot;topic&quot;</span><span class="token operator">:</span> <span class="token string">&quot;test_topic&quot;</span><span class="token punctuation">,</span>
+       
+       <span class="token property">&quot;producer_group&quot;</span><span class="token operator">:</span> <span class="token string">&quot;test_producer_group&quot;</span><span class="token punctuation">,</span>
+       <span class="token property">&quot;tag&quot;</span><span class="token operator">:</span> <span class="token string">&quot;itcase_test&quot;</span><span class="token punctuation">,</span>
+       <span class="token property">&quot;key&quot;</span><span class="token operator">:</span> <span class="token string">&quot;id&quot;</span><span class="token punctuation">,</span>
+       <span class="token property">&quot;partition_fields&quot;</span><span class="token operator">:</span> <span class="token string">&quot;id,date_field&quot;</span><span class="token punctuation">,</span>
+       
+       <span class="token property">&quot;columns&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+         <span class="token punctuation">{</span>
+           <span class="token property">&quot;index&quot;</span><span class="token operator">:</span> <span class="token number">0</span><span class="token punctuation">,</span>
+           <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;id&quot;</span><span class="token punctuation">,</span>
+           <span class="token property">&quot;type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;string&quot;</span>
+         <span class="token punctuation">}</span><span class="token punctuation">,</span>
+         <span class="token punctuation">{</span>
+           <span class="token property">&quot;index&quot;</span><span class="token operator">:</span> <span class="token number">1</span><span class="token punctuation">,</span>
+           <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;string_field&quot;</span><span class="token punctuation">,</span>
+           <span class="token property">&quot;type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;string&quot;</span>
+         <span class="token punctuation">}</span><span class="token punctuation">,</span>
+         <span class="token punctuation">{</span>
+           <span class="token property">&quot;index&quot;</span><span class="token operator">:</span> <span class="token number">2</span><span class="token punctuation">,</span>
+           <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;int_field&quot;</span><span class="token punctuation">,</span>
+           <span class="token property">&quot;type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;bigint&quot;</span>
+         <span class="token punctuation">}</span><span class="token punctuation">,</span>
+         <span class="token punctuation">{</span>
+           <span class="token property">&quot;index&quot;</span><span class="token operator">:</span> <span class="token number">3</span><span class="token punctuation">,</span>
+           <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;double_field&quot;</span><span class="token punctuation">,</span>
+           <span class="token property">&quot;type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;double&quot;</span>
+         <span class="token punctuation">}</span><span class="token punctuation">,</span>
+         <span class="token punctuation">{</span>
+           <span class="token property">&quot;index&quot;</span><span class="token operator">:</span> <span class="token number">4</span><span class="token punctuation">,</span>
+           <span class="token property">&quot;name&quot;</span><span class="token operator">:</span> <span class="token string">&quot;date_field&quot;</span><span class="token punctuation">,</span>
+           <span class="token property">&quot;type&quot;</span><span class="token operator">:</span> <span class="token string">&quot;date&quot;</span>
+         <span class="token punctuation">}</span>
+       <span class="token punctuation">]</span>
+     <span class="token punctuation">}</span>
+   <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4);function k(q,v){const a=u("RouterLink");return o(),e("div",null,[r,n("p",null,[s("父目录："),p(a,{to:"/zh/documents/connectors/rocketmq/rocketmq.html"},{default:c(()=>[s("RocketMQ 连接器")]),_:1})]),d])}const y=t(l,[["render",k],["__file","rocketmq-example.html.vue"]]);export{y as default};
