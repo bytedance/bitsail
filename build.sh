5a5afc1451e0ac0ef650b7d00f2692dd7eafd50e
liupeng.96 <96pengpeng@gmail.com>
2023-02-15 17:58:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -32,12 +32,12 @@ cp -r bitsail-dist/target/bitsail-dist-${revision}-bin/bitsail-archive-${revisio
 # add hadoop dependency for embedded flink
 if [[ ${mvnProfile} == "flink-embedded" ]]; then
   with_hadoop_version=$1
-  if [[ ${with_hadoop_version} == "with-hadoop2" ]]; then
+  if [[ ${with_hadoop_version} == "--with-hadoop2" ]]; then
     echo "Add flink-shaded-hadoop-2-uber dependency into embedded flink ..."
     HADOOP_UBER_URL=https://repo.maven.apache.org/maven2/org/apache/flink/flink-shaded-hadoop-2-uber/2.7.5-10.0/flink-shaded-hadoop-2-uber-2.7.5-10.0.jar
     wget -O output/embedded/flink/lib/flink-shaded-hadoop-2-uber.jar "${HADOOP_UBER_URL}"
   fi
-  if [[ ${with_hadoop_version} == "with-hadoop3" ]]; then
+  if [[ ${with_hadoop_version} == "--with-hadoop3" ]]; then
     echo "Add flink-shaded-hadoop-3-uber dependency into embedded flink ..."
     HADOOP_UBER_URL=https://repository.cloudera.com/artifactory/cloudera-repos/org/apache/flink/flink-shaded-hadoop-3-uber/3.1.1.7.2.9.0-173-9.0/flink-shaded-hadoop-3-uber-3.1.1.7.2.9.0-173-9.0.jar
     COMMONS_CLI_URL=https://repo1.maven.org/maven2/commons-cli/commons-cli/1.5.0/commons-cli-1.5.0.jar
