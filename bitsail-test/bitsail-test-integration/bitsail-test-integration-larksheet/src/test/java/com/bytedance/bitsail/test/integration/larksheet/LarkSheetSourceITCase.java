a3a6b8d3b5331a72115aecaf335ed58445a4ac63
liupeng.96 <96pengpeng@gmail.com>
2023-03-02 13:33:56 +0800

    Refactor larksheet itcase.

--- bitsail-test/bitsail-test-integration/bitsail-test-integration-larksheet/src/test/java/com/bytedance/bitsail/test/integration/larksheet/LarkSheetSourceITCase.java
+++ bitsail-test/bitsail-test-integration/bitsail-test-integration-larksheet/src/test/java/com/bytedance/bitsail/test/integration/larksheet/LarkSheetSourceITCase.java
@@ -14,24 +14,24 @@
  * limitations under the License.
  */
 
-package com.bytedance.bitsail.connector.larksheet.source;
+package com.bytedance.bitsail.test.integration.larksheet;
 
 import com.bytedance.bitsail.common.configuration.BitSailConfiguration;
 import com.bytedance.bitsail.connector.larksheet.option.LarkSheetReaderOptions;
-import com.bytedance.bitsail.test.connector.test.EmbeddedFlinkCluster;
-import com.bytedance.bitsail.test.connector.test.utils.JobConfUtils;
+import com.bytedance.bitsail.test.integration.AbstractIntegrationTest;
+import com.bytedance.bitsail.test.integration.utils.JobConfUtils;
 
 import org.junit.Ignore;
 import org.junit.Test;
 
 @Ignore
-public class LarkSheetSourceITCase {
+public class LarkSheetSourceITCase extends AbstractIntegrationTest {
 
   @Test
   public void testLarkSheetToPrint() throws Exception {
     BitSailConfiguration conf = JobConfUtils.fromClasspath("larksheet_to_print.json");
     addAppInfo(conf);
-    EmbeddedFlinkCluster.submitJob(conf);
+    submitJob(conf);
   }
 
   // TODO: add an available app for test
