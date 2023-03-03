19e335684e730bd5bec11910c88335e65e8d8df2
liupeng.96 <96pengpeng@gmail.com>
2023-03-03 16:30:03 +0800

    Refactor rocketmq legacy itcase.

--- bitsail-test/bitsail-test-integration/bitsail-test-integration-connector-legacy/bitsail-test-integration-rocketmq-legacy/src/test/java/com/bytedance/bitsail/test/integration/legacy/rocketmq/RocketMQOutputFormatITCase.java
+++ bitsail-test/bitsail-test-integration/bitsail-test-integration-connector-legacy/bitsail-test-integration-rocketmq-legacy/src/test/java/com/bytedance/bitsail/test/integration/legacy/rocketmq/RocketMQOutputFormatITCase.java
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-package com.bytedance.bitsail.connector.legacy.rocketmq.sink;
+package com.bytedance.bitsail.test.integration.legacy.rocketmq;
 
 import com.bytedance.bitsail.common.configuration.BitSailConfiguration;
 import com.bytedance.bitsail.connector.legacy.fake.option.FakeReaderOptions;
 import com.bytedance.bitsail.connector.legacy.rocketmq.option.RocketMQWriterOptions;
-import com.bytedance.bitsail.test.connector.test.EmbeddedFlinkCluster;
-import com.bytedance.bitsail.test.connector.test.utils.JobConfUtils;
+import com.bytedance.bitsail.test.integration.AbstractIntegrationTest;
+import com.bytedance.bitsail.test.integration.utils.JobConfUtils;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
@@ -37,11 +37,9 @@ import org.slf4j.LoggerFactory;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-/**
- * Need to start rocket service first.
- */
-@Ignore
-public class RocketMQOutputFormatITCase {
+@Ignore("Ignore unbounded streaming task.")
+public class RocketMQOutputFormatITCase extends AbstractIntegrationTest {
+
   private static final Logger LOG = LoggerFactory.getLogger(RocketMQOutputFormatITCase.class);
 
   private static final int TOTAL_SEND_COUNT = 300;
@@ -55,7 +53,7 @@ public class RocketMQOutputFormatITCase {
   public void testFakeToRocketMQ() throws Exception {
     BitSailConfiguration configuration = JobConfUtils.fromClasspath("fake_to_rocketmq.json");
     updateConfiguration(configuration);
-    EmbeddedFlinkCluster.submitJob(configuration);
+    submitJob(configuration);
 
     Queue<MessageExt> messages = consumeTopic();
     Assert.assertEquals(TOTAL_SEND_COUNT, messages.size());
