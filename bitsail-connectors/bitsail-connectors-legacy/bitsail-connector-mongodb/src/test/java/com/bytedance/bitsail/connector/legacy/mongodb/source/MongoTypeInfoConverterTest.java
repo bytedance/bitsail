2eba31d74dbf4a29fd9aac4a607e1bde28345a1b
liupeng.96 <96pengpeng@gmail.com>
2023-03-07 11:28:20 +0800

    Update MongoTypeInfoConverterTest.java
    Fix checkstyle.

--- bitsail-connectors/bitsail-connectors-legacy/bitsail-connector-mongodb/src/test/java/com/bytedance/bitsail/connector/legacy/mongodb/source/MongoTypeInfoConverterTest.java
+++ bitsail-connectors/bitsail-connectors-legacy/bitsail-connector-mongodb/src/test/java/com/bytedance/bitsail/connector/legacy/mongodb/source/MongoTypeInfoConverterTest.java
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-
 package com.bytedance.bitsail.connector.legacy.mongodb.source;
 
 import com.bytedance.bitsail.common.type.filemapping.FileMappingTypeInfoConverter;
